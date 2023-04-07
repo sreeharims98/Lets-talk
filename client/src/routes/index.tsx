8ad9d756767a9c6d124e1345c16e116f5d06a088
sreeharims <iamsreeharims@gmail.com>
2023-04-07 15:48:45 +0530

    client router changed to BrowserRouter

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,35 +1,21 @@
 import AuthPage from "../pages/AuthPage/AuthPage";
 import ChatPage from "../pages/ChatPage/ChatPage";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ProtectedRoute from "./ProtectedRoute";
 import UsersPage from "../pages/UsersPage/UsersPage";
 import { ROUTE_PATHS } from "../data/constants";
 import { HomeContainer } from "../pages/HomeContainer/HomeContainer";
-
+import { BrowserRouter, Routes as Routs, Route } from "react-router-dom";
 export const Routes = () => {
-  const router = createBrowserRouter([
-    {
-      path: ROUTE_PATHS.AUTH,
-      element: <AuthPage />,
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: ROUTE_PATHS.HOME,
-      element: <ProtectedRoute component={<HomeContainer />} />,
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          path: ROUTE_PATHS.USERS,
-          element: <ProtectedRoute component={<UsersPage />} />,
-        },
-        {
-          path: ROUTE_PATHS.CHAT,
-          element: <ProtectedRoute component={<ChatPage />} />,
-        },
-      ],
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <Routs>
+        <Route path={ROUTE_PATHS.AUTH} element={<AuthPage />} />
+        <Route path={ROUTE_PATHS.HOME} element={<ProtectedRoute component={<HomeContainer />} />}>
+          <Route path={ROUTE_PATHS.USERS} element={<ProtectedRoute component={<UsersPage />} />} />
+          <Route path={ROUTE_PATHS.CHAT} element={<ProtectedRoute component={<ChatPage />} />} />
+        </Route>
+      </Routs>
+    </BrowserRouter>
+  );
 };
